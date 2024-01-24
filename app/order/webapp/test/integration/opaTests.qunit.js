2daf9c1d8f4783e640d196bba42d1efb5d8dafea
Sameel Baker <sbaker@jdc-group.com>
2024-01-24 04:01:01 +0000

    orders app added

---
+++ app/order/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,28 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'order/test/integration/FirstJourney',
+		'order/test/integration/pages/OrdersList',
+		'order/test/integration/pages/OrdersObjectPage',
+		'order/test/integration/pages/OrderItemsObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, OrdersList, OrdersObjectPage, OrderItemsObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('order') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheOrdersList: OrdersList,
+					onTheOrdersObjectPage: OrdersObjectPage,
+					onTheOrderItemsObjectPage: OrderItemsObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
